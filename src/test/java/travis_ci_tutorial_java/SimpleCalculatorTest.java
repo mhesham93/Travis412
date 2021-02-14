02d60dcf1ce1ceb440bbee772dde396af2bca78a
mohamedsaid <mhesha1@students.towson.edu>
2021-02-14 13:53:41 -0500

    added unresolved functions (multiply, divide)

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -17,4 +17,18 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.minus(5, 3), 2);
 	}
+
+	@Test
+	public void testMultiply(){
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.multiply(2, 2), 4);
+
+	}
+
+	@Test
+	public void testDivide(){
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.divide(4, 4), 1);
+
+	}
 }
