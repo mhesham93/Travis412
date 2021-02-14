32ece499a885b0cf63ac7f4a31e82e0559d9c805
mohamedsaid <mhesha1@students.towson.edu>
2021-02-14 15:22:13 -0500

    changed the assertion value for testSubtract

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -15,7 +15,7 @@ public class SimpleCalculatorTest {
 	@Test
 	public void testSubtract(){
 		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.minus(5, 3), 2);
+		assertEquals(calc.minus(5, 3), 1);
 	}
 
 	@Test
